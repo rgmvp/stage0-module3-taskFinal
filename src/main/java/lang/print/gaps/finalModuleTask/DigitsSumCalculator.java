add25e35def152b381548dfb803173dc9e2c6daa
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-01-25 19:11:55 +0300

    done!~

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        System.out.println((int)(number / 1000) + (int) (number % 10) +  (int) (number / 10 % 10) +  (int)(number / 100 % 10));
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        System.out.println("" + (int) (number % 10) + "" + (int) (number % 100 / 10) + "" + (int)(number / 100));
     }
 }
